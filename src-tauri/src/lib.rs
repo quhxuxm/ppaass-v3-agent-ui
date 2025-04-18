cb236109c119d0f3d15b8faa9e8067e7b8eeea21
quh <quhxuxm_quh@163.com>
2025-04-18 16:39:58 +0800

    init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,16 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn start_agent(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn stop_agent(){
+
+}
+
+#[tauri::command]
+fn import_users(){}
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
